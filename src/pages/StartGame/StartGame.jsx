ec8182eb2e18441cacae52a3021f5d32729b30d4
Divyanshu Sahu <257divyanshu@gmail.com>
2025-07-27 17:26:41 +0530

    feat: implement word input form on start page

--- src/pages/StartGame/StartGame.jsx
+++ src/pages/StartGame/StartGame.jsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import TextInputFormContainer from "../../components/TextInputForm/TextInputFormContainer";
 
 function StartGame(){
+
+    const navigate = useNavigate();
+
+    function handleSubmission(){
+        navigate('/play');
+    }
+
     return (
         <>
             <div>Start Game page</div>
-            <Link to={'/play'}>Go to P  layGame page</Link>
+            <TextInputFormContainer onFormSubmit={handleSubmission}/>
+            {/* <Link to={'/play'}>Go to P  layGame page</Link> */}
         </>
     )
 }
