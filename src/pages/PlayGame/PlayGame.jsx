2fb5f1f7eed0b7765c741eec88761e778e5d95dd
Divyanshu Sahu <257divyanshu@gmail.com>
2025-07-27 17:20:16 +0530

    feat: implement navigation links between pages

--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function PlayGame(){
     return (
         <>
             <div>Play Game page</div>
+            <Link to={'/start'}>Go to StartGame page</Link>
         </>
     )
 }
--- src/pages/StartGame/StartGame.jsx
+++ src/pages/StartGame/StartGame.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function StartGame(){
     return (
         <>
             <div>Start Game page</div>
+            <Link to={'/play'}>Go to P  layGame page</Link>
         </>
     )
 }
