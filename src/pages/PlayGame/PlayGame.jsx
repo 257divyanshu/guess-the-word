3663a9be2bc605628565a5104097756825a9c51c
Divyanshu Sahu <257divyanshu@gmail.com>
2025-07-29 21:26:40 +0530

    feat: integrate masked text with on-screen keyboard

--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -1,22 +1,28 @@
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import LetterButtons from "../../components/LetterButtons/LetterButtons";
 import { useState } from "react";
+import MaskedText from "../../components/MaskedText/MaskedText";
 
-function PlayGame(){
+function PlayGame() {
     const [usedLetters, setUsedLetters] = useState([]);
     const location = useLocation();
     const wordSelected = location.state?.wordSelected;
-    function handleLetterClick(char){
-        setUsedLetters([...usedLetters, char]); 
+    function handleLetterClick(char) {
+        setUsedLetters([...usedLetters, char]);
     }
     return (
         <>
             <div>Play Game page</div>
-            <LetterButtons
-                word={wordSelected}
-                usedLetters={usedLetters}
-                onLetterClick={handleLetterClick}
-            />
+            <MaskedText word={wordSelected} usedLetters={usedLetters} />
+            <div className="flex justify-between items-center">
+                <div className="basis-2/4">
+                    <LetterButtons
+                        word={wordSelected}
+                        usedLetters={usedLetters}
+                        onLetterClick={handleLetterClick}
+                    />
+                </div>
+            </div>
         </>
     )
 }
