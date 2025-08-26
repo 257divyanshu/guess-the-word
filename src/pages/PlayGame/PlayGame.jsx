493fa756fd0e188ed8827c020a8b3239a6e9fb25
Divyanshu Sahu <257divyanshu@gmail.com>
2025-08-26 17:01:35 +0530

    feat: implement win detection logic

--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -8,7 +8,7 @@ function PlayGame() {
   const wordLength = location.state?.wordLength || 5;
   let wordSelected = location.state?.wordSelected;
   wordSelected = wordSelected.toUpperCase();
-  
+
   // State for the current, active guess being typed
   const [currentGuess, setCurrentGuess] = useState('');
 
@@ -17,26 +17,34 @@ function PlayGame() {
 
   let [blackChars, setBlackChars] = useState('');
 
-  const [guessGrid, setGuessGrid] = useState([false,false, false, false, false]);
+  const [guessGrid, setGuessGrid] = useState([false, false, false, false, false]);
   const [guessNo, setGuessNo] = useState(0);
-  
+
+  const [win, setWin] = useState(false);
+
   function handleKeyPress(key) {
+    if (win) return;
     if (key === "ENTER") {
+      let newGuessGrid = [...guessGrid];
+      newGuessGrid[guessNo] = true;
+      setGuessGrid(newGuessGrid);
+      if (currentGuess === wordSelected) {
+        console.log('you have won the game');
+        setWin(true);
+        return;
+      }
       // Only submit if the word is the correct length
       if (currentGuess.length === wordLength) {
         setSubmittedGuesses([...submittedGuesses, currentGuess]); // Add the guess to our list
         setCurrentGuess(''); // Clear the current guess for the next line
         let blackCharsToAdd = '';
-        for(let i = 0; i<wordLength; i++){
-          if(!wordSelected.includes(currentGuess[i])){
-            blackCharsToAdd+=currentGuess[i];
+        for (let i = 0; i < wordLength; i++) {
+          if (!wordSelected.includes(currentGuess[i])) {
+            blackCharsToAdd += currentGuess[i];
           };
         };
-        setBlackChars(blackChars+blackCharsToAdd);
-        let newGuessGrid = [...guessGrid];
-        newGuessGrid[guessNo] = true;
-        setGuessGrid(newGuessGrid);
-        setGuessNo(guessNo+1);
+        setBlackChars(blackChars + blackCharsToAdd);
+        setGuessNo(guessNo + 1);
       }
       return; // Stop further execution
     }
@@ -73,7 +81,7 @@ function PlayGame() {
         );
       })}
 
-      <KeyBoard onKeyPress={handleKeyPress} blackChars={blackChars}/>
+      <KeyBoard onKeyPress={handleKeyPress} blackChars={blackChars} />
     </>
   );
 }
