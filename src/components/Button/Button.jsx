29856e5465a3a3b1ba4a238f8f029b179849ec67
Divyanshu Sahu <257divyanshu@gmail.com>
2025-08-29 21:46:24 +0530

    refactor(Button): simplify styling and add click animations

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,23 @@
-import getBtnStyle from "./getBtnStyle";
-
-function Button({btnText, btnType, btnOnClick, btnStyle}){
+function Button({ btnText, btnType, btnOnClick, btnColor='blue'}) {
+    function handleClick() {
+        setTimeout(btnOnClick, 400);
+    };
+    const colorStyles = {
+    blue: 'bg-blue-500 hover:bg-blue-600',
+    green: 'bg-green-500 hover:bg-green-600',
+  };
     return (
         <button
-            className={`px-4 py-2 ${getBtnStyle(btnStyle)} text-white font-bold rounded-md transition-all`}
+            className={`
+        px-4 py-2 ${colorStyles[btnColor]} text-white font-bold rounded-md 
+        transition-all active:scale-95 w-full
+      `}
             type={btnType}
-            onClick={btnOnClick}
+            onClick={handleClick}
         >
             {btnText}
         </button>
-    )
+    );
 }
 
 export default Button;
